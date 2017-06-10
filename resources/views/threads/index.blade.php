d4324fb8d25537685f7b94b8d8202a6fc50bdfee
Leigh Dinaya <leighdinaya04@gmail.com>
2017-06-11 06:58:34 +0800

    move thread list to partials

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -5,16 +5,8 @@
 <div class="container">
 	<div class="col-md-8 col-md-offset-2">
 		<div class="list-group">
-		@forelse($threads as $thread)
-			<a href="#" class="list-group-item active">
-				<h4 class="list-group-item-heading">{{ $thread->subject}}</h4>
-				<p class="list-group-item-text">{{ str_limit($thread->thread, 100) }}</p>
-			</a>
-		@empty
-			<center>
-				<h3>No threads.</h3>
-			</center>
-		@endforelse
+		<h3>Recent Threads</h3>
+			@include('threads.partials.thread_list')
 		</div>
 	</div>	
 </div>
