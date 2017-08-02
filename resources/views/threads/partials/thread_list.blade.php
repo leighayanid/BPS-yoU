2469c13c5ccffc155f8d366327fcfa9de8c925b4
Leigh Dinaya <leighdinaya04@gmail.com>
2017-08-02 17:45:53 +0800

    edit style for navbar and list-group

--- resources/views/threads/partials/thread_list.blade.php
+++ resources/views/threads/partials/thread_list.blade.php
@@ -1,9 +1,9 @@
-<div class="list-group">
+<div class="list-group threads">
 	@forelse($threads as $thread)
 		<a href="{{ route('threads.show', $thread->id) }}" class="list-group-item active">
 			<span class="badge pull-right">{{ $thread->votes()->count() }} {{ str_plural('upvote', $thread->votes()->count() ) }}</span>
 			<h4 class="list-group-item-heading">{{ $thread->subject}}</h4>
-			<p class="list-group-item-text">{{ str_limit($thread->thread, 120) }}</p>
+			<p class="list-group-item-text muted">{{ str_limit($thread->thread, 200) }}</p>
 		</a>
 	@empty
 		<center>
