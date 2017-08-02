cba93d511604d2c34ae5db605c6e5394d9036c6a
Leigh Dinaya <leighdinaya04@gmail.com>
2017-08-02 18:08:56 +0800

    more styling

--- resources/views/threads/partials/thread_list.blade.php
+++ resources/views/threads/partials/thread_list.blade.php
@@ -2,8 +2,8 @@
 	@forelse($threads as $thread)
 		<a href="{{ route('threads.show', $thread->id) }}" class="list-group-item active">
 			<span class="badge pull-right">{{ $thread->votes()->count() }} {{ str_plural('upvote', $thread->votes()->count() ) }}</span>
-			<h4 class="list-group-item-heading">{{ $thread->subject}}</h4>
-			<p class="list-group-item-text muted">{{ str_limit($thread->thread, 200) }}</p>
+			<h4 class="list-group-item-heading">{{ ucfirst(trans($thread->subject))}}</h4>
+			<p class="list-group-item-text muted">{{ ucfirst(trans(str_limit($thread->thread, 200))) }}</p>
 		</a>
 	@empty
 		<center>
